78775ce3faf3613ada91c38ab29b399991de61e4
Shevchenko Taras <back.neomind@gmail.com>
2015-02-03 17:37:28 +0200

    After rotation box tutorial

--- core/src/com/tgs/thegame/Game.java
+++ core/src/com/tgs/thegame/Game.java
@@ -4,25 +4,45 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.asset.TextureKey;
 import com.jme3.material.Material;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.texture.Texture;
 
 public class Game extends SimpleApplication {
 
+    private Geometry boxGeometry;
+    private Node boxNode;
+
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
         setDisplayFps(false);
         setDisplayStatView(false);
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        TextureKey key = new TextureKey("Textures/Aladdin.jpg");
-        Texture tex = assetManager.loadTexture(key);
-        mat.setTexture("ColorMap", tex);
-        geom.setMaterial(mat);
+        Box box = new Box(1, 1, 1);
+        boxGeometry = new Geometry("Box", box);
 
-        rootNode.attachChild(geom);
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        TextureKey textureKey = new TextureKey("Textures/Jasmine.jpg");
+
+        Texture texture = assetManager.loadTexture(textureKey);
+        material.setTexture("ColorMap", texture);
+
+        boxGeometry.setLocalTranslation(0, 0, 2);
+        boxGeometry.setMaterial(material);
+
+
+        boxNode = new Node("boxNode");
+        boxNode.attachChild(boxGeometry);
+        boxNode.rotate(25, 25, 25);
+
+        rootNode.attachChild(boxNode);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+        boxGeometry.rotate(0.1f * tpf, 0.1f * tpf, 0.1f * tpf);
+        boxGeometry.move(0.1f * tpf, 0.1f * tpf, 0.1f * tpf);
+        boxNode.rotate(tpf, tpf, tpf);
     }
 }
